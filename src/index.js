41a1cdf79b010e68a6f51f32d24a9fb1674acfe4
Eyasu Lemma <eyasu.f.lemma.23@dartmouth.edu>
2023-04-12 21:50:40 -0400

    added favicon

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ let counter = 0;
 
 setInterval(() => {
   counter += 1;
-  $('#main').html('you have been on this page for ' + counter + ' seconds');
+  $('#main').html(`you have been on this page for ${counter} seconds`);
 }, 1000);
--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import eslint from 'vite-plugin-eslint';
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     eslint(),
+    vitePluginFaviconsInject('src/assets/nitya.jpg'),
   ],
   css: {
     postcss: {
