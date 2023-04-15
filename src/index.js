e76747d4bb56b134b6f36f06d985de21844c67e1
Eyasu Lemma <eyasu.f.lemma.23@dartmouth.edu>
2023-04-14 20:44:06 -0400

    added basic react

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles.scss';
-import $ from 'jquery';
-
-let counter = 0;
-
-setInterval(() => {
-  counter += 1;
-  $('#main').html(`you have been on this page for ${counter} seconds`);
-}, 1000);
