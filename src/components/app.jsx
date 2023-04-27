14fb2b8b08a4b627ebe161106fbb0c77d726fb70
nitya-cs <nitya.agarwala@gmail.com>
2023-04-26 22:46:21 -0400

    added redux

---
+++ src/components/app.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {
+  useParams,
+  BrowserRouter, Routes, Route, NavLink,
+} from 'react-router-dom';
+import '../style.scss';
+import Controls from './controls';
+import Counter from './counter';
+
+function Nav(props) {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return (
+    <div>Welcome
+      <Counter />
+      <Controls />
+    </div>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,56 +1,18 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import {
-  useParams,
-  BrowserRouter, Routes, Route, NavLink,
-} from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import './style.scss';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
+import App from './components/app';
 
-function Nav(props) {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
-  );
-}
-
-function About(props) {
-  return <div> All there is to know about me </div>;
-}
-function Welcome(props) {
-  return <div>Welcome</div>;
-}
-
-function Test(props) {
-  const { id } = useParams();
-  return <div> ID: {id} </div>;
-}
-
-function FallBack(props) {
-  return <div>URL Not Found</div>;
-}
-
-function App(props) {
-  return (
-    <BrowserRouter>
-      <div>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/test/:id" element={<Test />} />
-          <Route path="*" element={<FallBack />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
-}
+// this creates the store with the reducers
+const store = configureStore({
+  reducer: rootReducer,
+});
 
 const root = createRoot(document.getElementById('main'));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
